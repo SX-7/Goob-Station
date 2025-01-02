5ca54fbba48c2f2930298d4e0fc45cb1b38cc5c2
SX_7 <sn1.test.preria.2002@gmail.com>
2025-01-02 12:40:45 +0100

    Basic files for addiction

---
+++ Content.Shared/_Goobstation/Addiction/Components/AddictiveComponent.cs
@@ -0,0 +1,10 @@
+//using Content.Shared.Language;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._Goobstation.Addiction.Components;
+
+//[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[RegisterComponent, NetworkedComponent]
+public sealed partial class AddictiveComponent : Component
+{
+}
---
+++ Content.Shared/_Goobstation/Addiction/Systems/AddictionSystem.cs
@@ -0,0 +1,9 @@
+namespace Content.Shared._Goobstation.Addiction.Systems;
+
+public abstract class AddictionSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+    }
+}
