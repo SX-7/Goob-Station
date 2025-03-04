6bb31383401971d5ee67c7b0ea8e5d9036af6c1f
Roudenn <romabond091@gmail.com>
2025-03-04 09:32:18 +0300

    Port lavaland Content.Server

--- Content.Server/_Lavaland/Shuttles/Systems/DockingShuttleSystem.cs
+++ Content.Server/_Lavaland/Shuttles/Systems/DockingShuttleSystem.cs
@@ -9,7 +9,6 @@ using Robust.Shared.Map.Components;
 using System.Linq;
 using Content.Server.GameTicking;
 using Robust.Server.GameObjects;
-using Robust.Shared.Map;
 using Robust.Shared.Timing;
 
 namespace Content.Server._Lavaland.Shuttles.Systems;
@@ -29,7 +28,6 @@ public sealed class DockingShuttleSystem : SharedDockingShuttleSystem
         SubscribeLocalEvent<DockingShuttleComponent, FTLCompletedEvent>(OnFTLCompleted);
 
         SubscribeLocalEvent<StationGridAddedEvent>(OnStationGridAdded);
-        SubscribeLocalEvent<DockingShuttleComponent, ShuttleAddStationEvent>(OnAddStation);
     }
 
     private void OnMapInit(Entity<DockingShuttleComponent> ent, ref MapInitEvent args)
@@ -90,26 +88,5 @@ public sealed class DockingShuttleSystem : SharedDockingShuttleSystem
             Name = Name(station),
             Map = Transform(uid).MapID
         });
-    }
-
-    private void OnAddStation(EntityUid uid, DockingShuttleComponent component,  ShuttleAddStationEvent args)
-    {
-        component.Station = args.MapUid;
-        component.Destinations.Add(new DockingDestination()
-        {
-            Name = Name(args.MapUid),
-            Map = args.MapId
-        });
-    }
-}
-
-public sealed class ShuttleAddStationEvent : EntityEventArgs
-{
-    public readonly EntityUid MapUid;
-    public readonly MapId MapId;
-    public ShuttleAddStationEvent(EntityUid mapUid, MapId mapId)
-    {
-        MapUid = mapUid;
-        MapId = mapId;
     }
 }
